1a33e955dafa9521e17b9aa0ab1191b4b0468b86
Cuong Pham <cuongpham0212@gmail.com>
2026-06-09 00:27:47 +0700

    chore: remove Jinja and nested Hugo, keep single Hugo site

--- data.py
+++
@@ -1,6 +0,0 @@
-from site_data.config import site
-from site_data.content_vi import content as content_vi
-from site_data.content_en import content as content_en
-
-data_vi = {**site, **content_vi}
-data_en = {**site, **content_en}
