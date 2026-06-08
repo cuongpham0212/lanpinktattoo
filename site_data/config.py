202ba6d5182e5af93bafede1d08d2cd65be7bcf1
Cuong Pham <cuongpham0212@gmail.com>
2026-06-08 13:52:17 +0700

    Fix: update correct maps embed and social links

--- site_data/config.py
+++ site_data/config.py
@@ -5,10 +5,10 @@ site = {
     "phone": "0909 xxx xxx",
     "address": "Gần cầu vượt Tân Thới Hiệp, Quận 12, TP.HCM",
     "address_short": "Q.12, TP.HCM",
-    "maps_embed": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.297687683675!2d106.64560557518416!3d10.864947757554367!2m3!1f0!2f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3175297e1d5f90ad%3A0x6dcd055198206b25!2slanpink%20ink%20-%20Private%20Tattoo%20studio!5e0!3m2!1svi!2s!4v1780898799264!5m2!1svi!2s",
+    "maps_embed": "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3918.2976884391283!2d106.6456056!3d10.8649477!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3175297e1d5f90ad%3A0x6dcd055198206b25!2slanpink%20ink%20-%20Private%20Tattoo%20studio!5e0!3m2!1svi!2s!4v1780901452811!5m2!1svi!2s",
     "maps_link": "https://www.google.com/maps/dir//lanpink+ink+Private+Tattoo+studio/@10.8649477,106.6456056,17z",
-    "instagram": "https://instagram.com/lanpinktattoo",
-    "facebook": "https://facebook.com/lanpinktattoo",
+    "instagram": "https://www.instagram.com/lanpink.ink/",
+    "facebook": "https://www.facebook.com/LanPinkInk/",
     "og_image": "static/images/og-cover.jpg",
     "gallery": [
         {"img": "images/tattoo1.jpg", "alt": "Hình xăm mini hoa tại Quận 12"},
