09cee01bf016da4360c821cbaa2dc36d23027133
Cuong Pham <cuongpham0212@gmail.com>
2026-06-08 13:43:21 +0700

    Fix: rename data folder to site_data to avoid conflict

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
-from data.config import site
-from data.content_vi import content as content_vi
-from data.content_en import content as content_en
+from site_data.config import site
+from site_data.content_vi import content as content_vi
+from site_data.content_en import content as content_en
 
 data_vi = {**site, **content_vi}
 data_en = {**site, **content_en}
