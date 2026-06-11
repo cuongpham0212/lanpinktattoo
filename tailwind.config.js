a07cc75215eb021ba6ff65c85dca903f4c3b94af
Cuong Pham <cuongpham0212@gmail.com>
2026-06-11 13:52:22 +0700

    fix css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./layouts/**/*.html", "./content/**/*.md"],
+  content: [
+    "./layouts/**/*.html",
+    "./content/**/*.{html,md}",
+  ],
   theme: {
     extend: {},
   },
